e21a38fe124b9bf4e5422a952e478da4388cb315
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2022-04-14 19:48:24 -0400

    H2 database, test profile, JPA

--- src/main/java/com/systemmvp/sysorders/entities/User.java
+++ src/main/java/com/systemmvp/sysorders/entities/User.java
@@ -1,11 +1,17 @@
 package com.systemmvp.sysorders.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
-
     private static final long serialVersionUID = 8504984118443849722L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/systemmvp/sysorders/resources/UserResources.java
+++ src/main/java/com/systemmvp/sysorders/resources/UserResources.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.parser.Entity;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
