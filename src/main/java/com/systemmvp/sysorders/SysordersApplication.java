a0b8dbc3572484b8b5dbaea86bccaa17fdb9e761
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2022-04-14 18:39:05 -0400

    Project Created

---
+++ src/main/java/com/systemmvp/sysorders/SysordersApplication.java
@@ -0,0 +1,13 @@
+package com.systemmvp.sysorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysordersApplication.class, args);
+	}
+
+}
