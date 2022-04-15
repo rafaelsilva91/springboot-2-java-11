cc71bd0ab96067ad8a70eb1eff11942e7315ec0a
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2022-04-15 08:31:19 -0400

    OrderStatus enum created

--- src/main/java/com/systemmvp/sysorders/config/TestConfig.java
+++ src/main/java/com/systemmvp/sysorders/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.systemmvp.sysorders.config;
 
 import com.systemmvp.sysorders.entities.Order;
 import com.systemmvp.sysorders.entities.User;
+import com.systemmvp.sysorders.entities.enums.OrderStatus;
 import com.systemmvp.sysorders.repositories.OrderRepository;
 import com.systemmvp.sysorders.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +30,10 @@ public class TestConfig implements CommandLineRunner {
     	User u2 = new User(null, "Alex Ribeiro", "alex@gmail.com", "65992068888", "12345");
         User u3 = new User(null, "Ana Souza", "ana@gmail.com", "65992068877", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2022-01-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2022-02-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2022-03-22T15:21:22Z"), u1);
-        Order o4 = new Order(null, Instant.parse("2022-03-23T15:40:26Z"), u3);
+        Order o1 = new Order(null, Instant.parse("2022-01-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2022-02-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2022-03-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
+        Order o4 = new Order(null, Instant.parse("2022-03-23T15:40:26Z"),OrderStatus.WAITING_PAYMENT, u3);
 
     	userRepository.saveAll(Arrays.asList(u1,u2,u3));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4));
--- src/main/java/com/systemmvp/sysorders/entities/Order.java
+++ src/main/java/com/systemmvp/sysorders/entities/Order.java
@@ -1,7 +1,7 @@
 package com.systemmvp.sysorders.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.systemmvp.sysorders.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,6 +19,8 @@ public class Order implements Serializable {
     @JsonFormat(shape =JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -27,10 +29,12 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+
     }
 
     public Long getId() {
@@ -55,6 +59,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
