7ff2606955793d35704624144132053ac151b136
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2022-04-14 20:34:42 -0400

    Service Layer, component registration

--- src/main/java/com/systemmvp/sysorders/resources/UserResources.java
+++ src/main/java/com/systemmvp/sysorders/resources/UserResources.java
@@ -1,24 +1,33 @@
 package com.systemmvp.sysorders.resources;
 
 import com.systemmvp.sysorders.entities.User;
+import com.systemmvp.sysorders.services.UserService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "65992069999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 
